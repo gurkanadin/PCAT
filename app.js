3ee7785d1b51d28ca88590e37e6aa7c3636f9223
gurkanadin <gurkanadin@gmail.com>
2022-02-14 02:42:44 +0300

    Static Files Uploaded

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
-const fn = function () {
-  console.log('function working');
-};
+const express = require('express');
+const path = require('path');
+const app = express();
+
+//middleware
+app.use(express.static('public'));
+
+const port = 3000;
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Sunucu ${port} portunda başlatıldı`);
+});

e5862ce05d6d6ef60d59d76b88a121dd6a0d84d3
gurkanadin <gurkanadin@gmail.com>
2022-02-14 04:22:17 +0300

    Template Engine

--- app.js
+++ app.js
@@ -1,15 +1,27 @@
 const express = require('express');
+const ejs = require('ejs');
 const path = require('path');
+
 const app = express();
+
+// Template Engine
+app.set('view engine', 'ejs');
 
 //middleware
 app.use(express.static('public'));
 
-const port = 3000;
-app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'temp/index.html'));
-});
+//routing
 
+app.get('/', (req, res) => {
+  res.render('index');
+});
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+app.get('/add', (req, res) => {
+  res.render('add');
+});
+const port = 3000;
 app.listen(port, () => {
   console.log(`Sunucu ${port} portunda başlatıldı`);
 });
